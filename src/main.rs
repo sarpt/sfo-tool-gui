4dc560b93c2e6e4d246ec048868ee27adcd5b33e
sarpt <mike.m.info@gmail.com>
2025-09-13 17:16:58 +0200

    Separate sfo parsing into a mod outside main

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 use eframe::egui;
-use std::{
-  error::Error,
-  fs::OpenOptions,
-  io::{BufReader, Read},
-  path::PathBuf,
-};
+use std::{error::Error, fs::OpenOptions, io::BufReader, path::PathBuf};
 
 use clap::Parser;
-use thiserror::Error;
 
-use crate::sfo::{header::Header, index_table::IndexTable, mapping::Mapping};
+use crate::sfo::Sfo;
 
 mod sfo;
 
@@ -22,14 +16,6 @@ struct Args {
   input_file: PathBuf,
 }
 
-const UNCONTAINED_PARAM_SFO_MAGIC: [u8; 4] = [0x00, 0x50, 0x53, 0x46];
-
-#[derive(Error, Debug)]
-enum MainErr {
-  #[error("Provided file doesn't match known .sfo files magic")]
-  UnknownMagic,
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
   let args = Args::parse();
 
@@ -40,34 +26,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     .open(&args.input_file)?;
 
   let mut reader = BufReader::new(input_file);
-
-  let mut magic: [u8; 4] = [0; 4];
-  reader.read_exact(&mut magic)?;
-
-  println!("Magic: {:#04X?}", &magic);
-
-  if magic != UNCONTAINED_PARAM_SFO_MAGIC {
-    println!(
-      "Magic {:#04X?} doesn't match any known .sfo file magic",
-      &magic
-    );
-    return Err(Box::new(MainErr::UnknownMagic));
-  }
-
-  let header = Header::new(&mut reader)?;
-  println!("{header}");
-
-  let index_table = IndexTable::new(&mut reader, &header)?;
-  println!("{index_table}");
-
-  let entries_mapping = Mapping::new(&mut reader, &index_table)?;
-  println!("{entries_mapping}");
+  let sfo = Sfo::new(&mut reader).map_err(Box::new)?;
 
   let native_options = eframe::NativeOptions::default();
   eframe::run_native(
     "Read .sfo",
     native_options,
-    Box::new(|cc| Ok(Box::new(GuiApp::new(cc, entries_mapping)))),
+    Box::new(|cc| Ok(Box::new(GuiApp::new(cc, sfo)))),
   )
   .map_err(|err| format!("could not start eframe application: {err}"))?;
 
@@ -75,16 +40,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 struct GuiApp {
-  mapping: Mapping,
+  sfo: Sfo,
 }
 
 impl GuiApp {
-  fn new(_cc: &eframe::CreationContext<'_>, mapping: Mapping) -> Self {
-    GuiApp { mapping }
+  fn new(_cc: &eframe::CreationContext<'_>, sfo: Sfo) -> Self {
+    GuiApp { sfo }
   }
 
   fn mapping_entries_grid(&self, ui: &mut eframe::egui::Ui) {
-    for (key, value) in &self.mapping.entries {
+    for (key, value) in &self.sfo.entries_mapping.entries {
       ui.label(key.to_string());
       ui.label(value.to_string());
       ui.end_row();
--- src/sfo/mod.rs
+++ src/sfo/mod.rs
@@ -1,5 +1,64 @@
+use std::io::Read;
+use thiserror::Error;
+
+use crate::sfo::{header::Header, index_table::IndexTable, mapping::Mapping};
+
 pub mod format;
 pub mod header;
 pub mod index_table;
 pub mod keys;
 pub mod mapping;
+
+pub struct Sfo {
+  pub header: Header,
+  pub index_table: IndexTable,
+  pub entries_mapping: Mapping,
+}
+
+const UNCONTAINED_PARAM_SFO_MAGIC: [u8; 4] = [0x00, 0x50, 0x53, 0x46];
+
+#[derive(Error, Debug)]
+pub enum SfoParseErr {
+  #[error("Could not read entries mappings: {0}")]
+  EntriesMappingReadErr(String),
+  #[error("Could not read index table: {0}")]
+  IndexTableReadErr(String),
+  #[error("Could not read header: {0}")]
+  HeaderReadErr(String),
+  #[error("Could not read magic: {0}")]
+  MagicReadErr(String),
+  #[error("Provided file doesn't match known .sfo files magic: {0}")]
+  UnknownMagic(String),
+}
+
+impl Sfo {
+  pub fn new<T>(reader: &mut T) -> Result<Self, SfoParseErr>
+  where
+    T: Read,
+  {
+    let mut magic: [u8; 4] = [0; 4];
+    reader
+      .read_exact(&mut magic)
+      .map_err(|err| SfoParseErr::MagicReadErr(err.to_string()))?;
+
+    println!("Magic: {:#04X?}", &magic);
+
+    if magic != UNCONTAINED_PARAM_SFO_MAGIC {
+      return Err(SfoParseErr::UnknownMagic(format!(
+        "Magic {:#04X?} doesn't match any known .sfo file magic",
+        &magic
+      )));
+    }
+
+    let header = Header::new(reader).map_err(SfoParseErr::HeaderReadErr)?;
+    let index_table = IndexTable::new(reader, &header).map_err(SfoParseErr::IndexTableReadErr)?;
+    let entries_mapping =
+      Mapping::new(reader, &index_table).map_err(SfoParseErr::EntriesMappingReadErr)?;
+
+    Ok(Self {
+      header,
+      index_table,
+      entries_mapping,
+    })
+  }
+}
