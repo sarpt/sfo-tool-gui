c8cf06e231158eea256c9d533ba528830b574abf
sarpt <mike.m.info@gmail.com>
2025-09-15 21:36:22 +0200

    Show index table entry info on key hover

--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,5 @@
 use crate::sfo::Sfo;
-use eframe::egui;
+use eframe::egui::{self};
 
 pub struct GuiApp {
   sfo: Sfo,
@@ -11,24 +11,37 @@ impl GuiApp {
   }
 
   fn mapping_entries_grid(&self, ui: &mut eframe::egui::Ui) {
-    for (key, entry) in self.sfo.iter() {
-      ui.label(key.to_string());
-      ui.label(entry.data.to_string());
-      ui.end_row();
-    }
+    egui::Grid::new("mapping_grid")
+      .num_columns(2)
+      .min_col_width(120.0)
+      .max_col_width(ui.available_size().x)
+      .spacing([40.0, 4.0])
+      .striped(true)
+      .show(ui, |ui| {
+        ui.label("KEY");
+        ui.label("DATA");
+        ui.end_row();
+
+        ui.label("");
+        ui.add_sized(ui.available_size(), egui::Label::new(""));
+        ui.end_row();
+
+        for (key, entry) in self.sfo.iter() {
+          ui.label(key.to_string())
+            .on_hover_text(entry.index_table_entry.to_string());
+          ui.label(entry.data.to_string());
+          ui.end_row();
+        }
+      });
   }
 }
 
 impl eframe::App for GuiApp {
   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
     egui::CentralPanel::default().show(ctx, |ui| {
-      egui::Grid::new("mapping_grid")
-        .num_columns(2)
-        .spacing([40.0, 4.0])
-        .striped(true)
-        .show(ui, |ui| {
-          self.mapping_entries_grid(ui);
-        });
+      egui::ScrollArea::both().show(ui, |ui| {
+        self.mapping_entries_grid(ui);
+      });
     });
   }
 }
