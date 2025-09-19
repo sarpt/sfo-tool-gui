9b29cff211349a44f628fdcce82a574e3a073dfb
sarpt <mike.m.info@gmail.com>
2025-09-19 20:18:32 +0200

    Add rfd & show file dialog when path to sfo is not being provided by cli arguments

--- src/gui.rs
+++ src/gui.rs
@@ -1,16 +1,31 @@
+use std::{fs::OpenOptions, io::BufReader, path::Path};
+
 use crate::sfo::Sfo;
 use eframe::egui::{self};
+use rfd::FileDialog;
 
 pub struct GuiApp {
-  sfo: Sfo,
+  sfo: Option<Sfo>,
 }
 
 impl GuiApp {
-  pub fn new(_cc: &eframe::CreationContext<'_>, sfo: Sfo) -> Self {
+  pub fn new<T>(_cc: &eframe::CreationContext<'_>, path: Option<&T>) -> Self
+  where
+    T: AsRef<Path>,
+  {
+    let sfo = path.and_then(|path| {
+      load_sfo_file(path.as_ref()).map_or_else(
+        |err| {
+          println!("could not load sfo file provided in arguments: {err}");
+          None
+        },
+        Some,
+      )
+    });
     GuiApp { sfo }
   }
 
-  fn mapping_entries_grid(&self, ui: &mut eframe::egui::Ui) {
+  fn mapping_entries_grid(&self, ui: &mut eframe::egui::Ui, sfo: &Sfo) {
     egui::Grid::new("mapping_grid")
       .num_columns(2)
       .min_col_width(120.0)
@@ -26,7 +41,7 @@ impl GuiApp {
         ui.add_sized(ui.available_size(), egui::Label::new(""));
         ui.end_row();
 
-        for (key, entry) in self.sfo.iter() {
+        for (key, entry) in sfo.iter() {
           ui.label(key.to_string())
             .on_hover_text(entry.index_table_entry.to_string());
           ui.label(entry.data.to_string());
@@ -38,10 +53,50 @@ impl GuiApp {
 
 impl eframe::App for GuiApp {
   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-    egui::CentralPanel::default().show(ctx, |ui| {
-      egui::ScrollArea::both().show(ui, |ui| {
-        self.mapping_entries_grid(ui);
-      });
-    });
+    match &self.sfo {
+      Some(sfo) => {
+        egui::CentralPanel::default().show(ctx, |ui| {
+          egui::ScrollArea::both().show(ui, |ui| {
+            self.mapping_entries_grid(ui, sfo);
+          });
+        });
+      }
+      None => {
+        let files = FileDialog::new()
+          .add_filter("Sfo", &["sfo", "SFO"])
+          .set_directory("/")
+          .pick_file();
+
+        let data_path = match files {
+          Some(path) => path,
+          None => {
+            println!("no path provided");
+            return;
+          }
+        };
+        self.sfo = load_sfo_file(&data_path).map_or_else(
+          |err| {
+            println!("could not load a sfo file: {err}");
+            None
+          },
+          Some,
+        );
+        ctx.request_repaint();
+      }
+    }
   }
+}
+
+fn load_sfo_file<T>(path: T) -> Result<Sfo, String>
+where
+  T: AsRef<Path>,
+{
+  let file = OpenOptions::new()
+    .read(true)
+    .write(false)
+    .open(path.as_ref())
+    .map_err(|err| format!("could not load file: {err}"))?;
+
+  let mut reader = BufReader::new(file);
+  Sfo::new(&mut reader).map_err(|err| format!("could not load file: {err}"))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::{error::Error, fs::OpenOptions, io::BufReader, path::PathBuf};
+use std::{error::Error, path::PathBuf};
 
 use clap::Parser;
 
-use crate::{gui::GuiApp, sfo::Sfo};
+use crate::gui::GuiApp;
 
 mod gui;
 mod sfo;
@@ -12,27 +12,17 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 #[derive(Parser, Debug)]
 #[command(version = VERSION, about = "analysis of PS3 .sfo files", long_about = None)]
 struct Args {
-  #[arg(long, required = true, help = "Path to a .sfo file")]
-  input_file: PathBuf,
+  #[arg(long, required = false, help = "Path to a .sfo file")]
+  input_file: Option<PathBuf>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
   let args = Args::parse();
-
-  let input_file = OpenOptions::new()
-    .read(true)
-    .write(false)
-    .create(false)
-    .open(&args.input_file)?;
-
-  let mut reader = BufReader::new(input_file);
-  let sfo = Sfo::new(&mut reader).map_err(Box::new)?;
-
   let native_options = eframe::NativeOptions::default();
   eframe::run_native(
     "Read .sfo",
     native_options,
-    Box::new(|cc| Ok(Box::new(GuiApp::new(cc, sfo)))),
+    Box::new(|cc| Ok(Box::new(GuiApp::new(cc, args.input_file.as_ref())))),
   )
   .map_err(|err| format!("could not start eframe application: {err}"))?;
 
